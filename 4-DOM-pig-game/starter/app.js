77d2431dc02f075c5e25b901050adbf6bd29c430
christian vorgitch <vorgitchchristian@gmail.com>
2020-06-29 22:25:11 -0500

    update
    added winner function to player status

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -46,6 +46,44 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 	} else {
 		// Next player
+		nextPlayer();
+
+	}
+
+
+
+
+
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	//add CURRENT score to GLOBAL sore
+	scores[activePlayer] += roundScore;
+
+
+
+	// Update the UI 
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	// Check if player won the game
+	if (scores[activePlayer] >= 100) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
+
+	} else {
+		nextPlayer();
+	}
+
+
+	// Next Player
+	nextPlayer();
+})
+
+function nextPlayer() {
+	// Next player
 		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 		// if activePlayer is = to 0 then switch to 1 or vice versa
 		roundScore = 0; 
@@ -61,16 +99,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		//document.querySelector('.player-1-panel').classList.add('active');
 
 		document.querySelector('.dice').style.display = 'none';
-
-	}
-
-
-
-
-
-});
-
-
+}
 
 
 
